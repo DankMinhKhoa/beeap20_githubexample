1dbcbfefd8e7c8f7b1a39c6f5210d750e46e65a5
Khoa Dang <minhkhoadang@gmail.com>
2021-04-07 11:06:09 +0300

    Revert "Ilia's thoughts"

--- beeap20.py
+++ beeap20.py
@@ -1,8 +1,6 @@
 # todo: rename the writeYourThought function to yourname_writeYourThought
 # print whatever you want to say to the rest of your peers
 # no nsfw!
-def IliasThought():
-    print("GIT is a thing we should know.")
+def writeYourThought():
+    print("your Thoughts")
     
-
-IliasThought()
